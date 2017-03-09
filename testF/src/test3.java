bb40c716e06b09b2925a09ebd74ebefd0368ed40
adamly <adamlyz@hotmail.com>
2017-03-09 17:06:16 +0800

    65656

--- testF/src/test3.java
+++ testF/src/test3.java
@@ -9,7 +9,7 @@ import static java.lang.System.*;
 public class test3 {
     public static void main(String[] args) {
     	
-    	System.out.println("test3");;
+    	System.out.println("test33");;
     
     }
 }
