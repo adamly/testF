d87ddac27b05c877f2f628afd68a768ccf2a2d5f
adamly <adamlyz@hotmail.com>
2017-03-09 17:06:03 +0800

    656

--- testF/src/test2.java
+++ testF/src/test2.java
@@ -9,7 +9,7 @@ import static java.lang.System.*;
 public class test2 {
     public static void main(String[] args) {
     	
-    	System.out.println("test3");;
+    	System.out.println("test22");;
     
     }
 }
